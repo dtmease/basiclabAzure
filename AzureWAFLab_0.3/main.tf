4ad1cd94d065e2603746f95b7cfbf5ecab94dede
dtmease <110178802+dtmease@users.noreply.github.com>
2023-07-23 19:39:55 +0100

    Update main.tf

--- AzureWAFLab_0.3/main.tf
+++ AzureWAFLab_0.3/main.tf
@@ -139,7 +139,7 @@ resource "azurerm_public_ip" "pip-waf" {
   resource_group_name = azurerm_resource_group.rgDVWA.name
   location            = azurerm_resource_group.rgDVWA.location
   allocation_method   = "Dynamic"
-  sku                 = "Standard"
+  sku                 = "Basic"
 }
 
 resource "azurerm_application_gateway" "dvwa-waf" {
